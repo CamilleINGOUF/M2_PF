d88c0e94c40c010080530a5af8f5b0c2fd20adfe
CamilleINGOUF <ingouf.camille@gmail.com>
2018-10-15 15:54:55 +0200

    added types

--- Haskell/liste_recherche.hs
+++ Haskell/liste_recherche.hs
@@ -1,14 +1,21 @@
-search::(Eq a) => [a] -> a -> Bool
-search [] _ = False
+data Result a = Found a | NotFound
+
+instance Show a => Show (Result a) where
+    show (Found a) = show a
+    show (NotFound) = "[Not Found]"
+
+search::(Eq a) => [a] -> a -> Result a
+search [] _ = NotFound
 search (x:xs) v
-    | x == v = True
+    | x == v = Found x
     | otherwise = search xs v
 
-searchValue::(Eq a) => [(a,b)] -> a -> b
+searchValue::(Eq a) => [(a,b)] -> a -> Result b
+searchValue [] _ = NotFound
 searchValue (x:xs) k
-    | k == (fst x) = (snd x)
+    | k == (fst x) = Found (snd x)
     | otherwise = (searchValue xs k)
 
 main = do
-    print $ search [1,2,3,4] 9
+    print $ search [1,2,3,4] 5
     print $ searchValue [(1,'a'),(2,'b'),(3,'c')] 3
