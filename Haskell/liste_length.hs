1e601526d5de6b8a8368b04f4ab3e8cc6c150251
CamilleINGOUF <ingouf.camille@gmail.com>
2018-10-15 15:55:19 +0200

    added function

--- Haskell/liste_length.hs
+++ Haskell/liste_length.hs
@@ -1,6 +1,10 @@
 length'::(Floating a) => [(a,a)] -> a
-length' [x] = 0
-length' (x:y:xs) = sqrt ((fst y - fst x)^2 + (snd y - snd x)^2) + length' (y:xs)
+length' [] = 0
+length' [_] = 0
+length' (x:y:xs) = (len x y) + length' (y:xs)
+
+len:: (Floating a) => (a, a) -> (a, a) -> a
+len x y = sqrt ((fst y - fst x)^2 + (snd y - snd x)^2)
 
 main = do
-    print $ length' [(1,2),(8,7),(6,6)]
+    print $ length' [(0,0),(3,4),(13,4)]
