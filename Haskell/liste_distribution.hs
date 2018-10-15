ab94d76351c00802238913fbc7ccbe4f9b1848f3
CamilleINGOUF <ingouf.camille@gmail.com>
2018-10-15 13:32:37 +0200

    liste_districution Terminal

--- Haskell/liste_distribution.hs
+++ Haskell/liste_distribution.hs
@@ -1,2 +1,9 @@
-halfDeck::[a] -> ([a],[a])
-halfDeck 
+halfDeckT::[a] -> [a] -> [[a]]
+halfDeckT [] _ = []
+halfDeckT (x:xs) l
+    | length xs == length l = [ xs , x:l ]
+    | length (x:xs) == length l = [ x:xs , l ]
+    | otherwise = halfDeckT xs (x:l)
+
+main = do
+    print $ halfDeckT [1,2,3,4,5,6,7] []
