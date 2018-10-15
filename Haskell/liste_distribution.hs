b1b698d872ad015a8b446d4c241977afba4eb525
CamilleINGOUF <ingouf.camille@gmail.com>
2018-10-15 15:55:08 +0200

    added algos

--- Haskell/liste_distribution.hs
+++ Haskell/liste_distribution.hs
@@ -5,5 +5,16 @@ halfDeckT (x:xs) l
     | length (x:xs) == length l = [ x:xs , l ]
     | otherwise = halfDeckT xs (x:l)
 
+halfDeckTT::[a] -> [a] -> [a]-> ([a],[a])
+halfDeckTT [] i j = (i,j)
+halfDeckTT (x:xs) i j = halfDeckTT xs j (x:i)
+
+halfDeck::[a] -> ([a],[a])
+halfDeck [] = ([],[])
+halfDeck (x:xs) = (x:j,i)
+    where (i,j) = halfDeck xs
+
 main = do
     print $ halfDeckT [1,2,3,4,5,6,7] []
+    print $ halfDeckTT [1,2,3,4,5,6,7] [] []
+    print $ halfDeck [1,2,3,4,5,6]
